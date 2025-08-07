22f4676059f0d5e23dfcc3ffbf092e1a753ec0d7
HaziqHasan <hasanhaziq5@gmail.com>
2025-08-07 20:55:22 +0530

    changed styling

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { BellIcon, Bars3Icon, ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, ShoppingCartIcon } from "@heroicons/react/24/outline";
 import { NavLink, useNavigate } from "react-router-dom";
 import LoginPage from "../SignIn&SingUp/LoginPage";
 import SignUpPage from "../SignIn&SingUp/SignUpPage";
@@ -106,7 +105,9 @@ export default function Navbar() {
             className="rounded-full p-2 hover:bg-white/10 relative"
             onClick={handlecart}
           >
-            <ShoppingCartIcon className="h-6 w-6" />
+           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+</svg>
             {cart.length > 0 && (
               <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
                 {cart.length}
